1b32d2dd99b65ca5ef02e31d05c91c217ad31a17
Vedanshi Dubey <131689978+Vedanshidubey2005@users.noreply.github.com>
2025-06-06 23:57:36 +0530

    Create First.js

---
+++ Basics/First.js
@@ -0,0 +1,25 @@
+console.log("This is my first javascript program");
+var a;
+var b =8;
+// console.log(a);   undefined
+a=9;
+a=b;
+console.log(a);
+console.log(b);
+var c = 10;
+var d=5;
+var e ="Vedanshi ";
+
+c=c+10;
+d=d+5;
+e=e+("Dubey");
+console.log(c);
+console.log(d);
+console.log(e);
+
+// This is my first javascript program
+// 8
+// 8
+// 20
+// 10
+// Vedanshi Dubey
