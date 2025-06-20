2346861541ca6b874b678d8cc53b206b0a5d4e8e
Vedanshi Dubey <131689978+Vedanshidubey2005@users.noreply.github.com>
2025-06-20 23:53:26 +0530

    Update First.js

--- Basics/First.js
+++ Basics/First.js
@@ -23,3 +23,30 @@ console.log(e);
 // 20
 // 10
 // Vedanshi Dubey
+
+//parseint function 
+
+function convertToInt(str){
+    return parseInt(str);
+}
+console.log(convertToInt("78"));  //78
+
+function convertToInt2(str){
+    return parseInt(str,2);
+}
+console.log(convertToInt2("101"));      //5
+
+//ternary operator
+
+function vote(age){
+  return  age>=18 ? "eligible": "not eligible";
+}
+console.log( vote(56));               //eligible
+
+//multiple ternary operators 
+function checksign(val){
+    return val> 0 ? "positive" : val<0 ? "negative": "zero";
+}
+console.log( checksign(0));      //zero
+console.log( checksign(-9));         //negative
+console.log( checksign(9));            //positive
