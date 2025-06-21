8407e567be03f097efe5e11a9951adfd9e06f4b7
Vedanshi Dubey <131689978+Vedanshidubey2005@users.noreply.github.com>
2025-06-21 22:10:57 +0530

    Update First.js

--- Basics/First.js
+++ Basics/First.js
@@ -100,3 +100,25 @@ function edit(){
 }
 edit();
 console.log(s);             //[ 1, 2, 3 ]
+
+//prevent form chnage - Object.freeze()
+
+function freezeobj(){
+    "use strict";
+    const MATH_CONSTANTS={
+        PI: 3.14
+    }
+    Object.freeze(MATH_CONSTANTS);
+    try{
+        MATH_CONSTANTS.PI=99;
+    }
+    catch(ex){
+        console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+}
+
+const PI = freezeobj();
+console.log(PI);
+//TypeError: Cannot assign to read only property 'PI' of object '#<Object>'
+//3.14
