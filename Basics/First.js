cf3be3003917eda66c6da6d17ce6c26e8fe84bde
Vedanshi Dubey <131689978+Vedanshidubey2005@users.noreply.github.com>
2025-06-21 22:00:20 +0530

    Update First.js

--- Basics/First.js
+++ Basics/First.js
@@ -50,3 +50,53 @@ function checksign(val){
 console.log( checksign(0));      //zero
 console.log( checksign(-9));         //negative
 console.log( checksign(9));            //positive
+
+//let
+let n1 = "jerry";
+// let n2;                 //SyntaxError: Identifier 'n1' has already been declared
+n1= "sherry";
+console.log(n1);          //sherry
+
+//scope
+function scope(){
+    "use strict";
+    //  var i = "function scope";
+     if(true){
+       var i="block scope";
+        console.log("block scope of i is",i)
+     }
+     console.log("function scope of i is", i);  //accessible outside if block
+     return i;
+}
+scope();
+//block scope of i is block scope
+// function scope of i is block scope
+
+function scope1(){
+    "use strict";
+     let i1 = "function scope";
+     if(true){
+       let i1="block scope";
+        console.log("block scope of i is",i1)
+     }
+     console.log("function scope of i is", i1);
+     return i1;
+    }
+scope1();
+// block scope of i is block scope
+// function scope of i is function scope
+
+//const keyword --> cannot be reassigned
+
+const str1 = "ert";
+//str1="wer";            //TypeError: Assignment to constant variable.
+
+//chnage array val declared with const
+const s= [3,7,5];
+function edit(){
+    s[0]=1;
+    s[1]=2;
+    s[2]=3;
+}
+edit();
+console.log(s);             //[ 1, 2, 3 ]
